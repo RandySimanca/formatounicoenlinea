d926c529db8a6df37782b5a7993682ca6e33a545
Randy Simanca <randysimancamercado@gmail.com>
2025-10-24 18:06:21 -0500

    coordenadas de tipo documento

--- frontend/src/composables/useFormatoOficialHV.js
+++ frontend/src/composables/useFormatoOficialHV.js
@@ -61,9 +61,9 @@ export function useFormatoOficialHV() {
         if (datosUsuario.tipoDocumento === "C.C") {
           write(page1, "X", 82, 220, 10, fontBold);
         } else if (datosUsuario.tipoDocumento === "C.E") {
-          write(page1, "X", 82, 208, 10, fontBold);
+          write(page1, "X", 85, 220, 10, fontBold);
         } else if (datosUsuario.tipoDocumento === "PAS") {
-          write(page1, "X", 125, 208, 10, fontBold);
+          write(page1, "X", 95, 220, 10, fontBold);
         }
 
         write(page1, datosUsuario.numDocumento || "", 185, 220, fontSize + 1);
